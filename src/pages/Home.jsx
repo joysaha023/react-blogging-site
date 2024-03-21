b2151a3fc01f5f34e01bc073f00bebd120d4223f
Joy <github.joysaha@gmail.com>
2024-03-21 16:49:00 +0600

    navbar added from deisy ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h2>This is a home page </h2>
         </div>
     );
