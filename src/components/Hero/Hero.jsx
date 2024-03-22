cb2222259067f1baef4304e1d06beace3b699937
Joy <github.joysaha@gmail.com>
2024-03-22 21:06:03 +0600

    home section button added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div>
       <div className="hero  bg-base-200">
         <div className="hero-content text-center">
-          <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Welcome to BistroBlog</h1>
+          <div className="max-w-lg">
+            <h1 className="text-5xl font-bold">
+              Welcome to <span>BistroBlog</span>
+            </h1>
             <p className="py-6">
-            BistroBlog is the bridge between the complex world of technology and the curious minds eager to understand it
+              BistroBlog is the bridge between the complex world of technology
+              and the curious minds eager to understand it
             </p>
-            <button className="btn btn-primary">Read Blog</button>
-            <button className="btn btn-primary">Bookmarks</button>
+            <div className="flex gap-2 justify-center">
+              <Link
+                to="/blogs"
+                className="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
+                Read Blogs
+                </span>
+              </Link>
+              <Link
+                to="/bookmarks"
+                className="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+                <span className="relative text-black group-hover:text-white">
+                Bookmarks
+                </span>
+              </Link>
+              
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Hero from '../components/Hero/Hero';
 
 const Home = () => {
     return (
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center min-h-[calc(100vh-116px)]'>
             <Hero></Hero>
         </div>
     );
