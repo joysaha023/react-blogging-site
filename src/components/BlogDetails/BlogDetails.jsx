0673eb330ee1591a20e44df4d32b92831f9001bb
Joy <github.joysaha@gmail.com>
2024-03-28 20:51:34 +0600

    create localstoragae and save data

--- src/components/BlogDetails/BlogDetails.jsx
+++ src/components/BlogDetails/BlogDetails.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import SingleBlogDe from "../SingleBlogDe";
 
 const BlogDetails = () => {
-    const blogs = useLoaderData();
+  const blogs = useLoaderData();
+  const { id } = useParams();
+  const blog = blogs.find((blog) => blog.id == id);
+
+ const handleBookmark = () => {
+    console.log("click")
+ }
+
+
   return (
     <div className="max-w-6xl mx-auto mt-10">
-      {
-        blogs.map((blog) =>  )
-      }
-      
+      <div className="card w-full bg-base-100 shadow-none">
+        <figure>
+          <img
+            src={blog.cover_image}
+            alt="Ses"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{blog.title}</h2>
+          <p>{blog.description} fff</p>
+          <div className="card-actions justify-end">
+            <button onClick={handleBookmark} className="btn text-white btn-primary">book Mark</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/SingleBlogDe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SingleBlogDe = () => {
+    return (
+        <div>
+            <h3>hello guis
+                
+            </h3>
+        </div>
+    );
+};
+
+export default SingleBlogDe;
