407660cc7fa00b4c660431936fbb03feb18c3a26
Joy <github.joysaha@gmail.com>
2024-03-28 20:23:32 +0600

    new card added

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import errImage from '../../assets/404.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import errImage from "../../assets/404.jpg";
 
-const BlogCard = ({blog}) => {
-    const {cover_image, title, description, published_at, id} = blog
-    return (
-        <div>
-            <Link to='/blog/${id}' href="#" className="max-w-sm mx-auto transition border-2 hover:scale-105 border-primary border-opacity-30 hover:border-secondary group hover:no-underline focus:no-underline bg-gray-900">
-                    <img role="presentation" className="object-cover w-full rounded h-44 bg-gray-500" src={cover_image || errImage} />
-                    <div className="p-6 space-y-2">
-                        <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{title}</h3>
-                        <span className="text-xs text-gray-400">{new Date(published_at).toLocaleString()}</span>
-                        <p>{description}</p>
-                    </div>
-            </Link>
+const BlogCard = ({ blog }) => {
+  const { cover_image, title, description, published_at, id } = blog;
+  return (
+    <div>
+      
+      <Link to="/blog/${id}"  className="card h-full w-full bg-base-100 shadow-xl">
+        <figure className="h-full m-6">
+          <img
+            src={cover_image || errImage}
+            alt="Shoes"
+            className=""
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
         </div>
-    );
+      </Link>
+    </div>
+  );
 };
 
 export default BlogCard;
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -4,18 +4,10 @@ import BlogCard from '../components/BlogCard/BlogCard';
 const Blogs = () => {
 
     const blogs = useLoaderData()
-    console.log(blogs)
+   
     return (
-        <section className="bg-gray-800 py-6 text-gray-100">
         <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-            <a rel="noopener noreferrer" href="#" className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900">
-                <img src={blogs[0].cover_image} alt="" className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500" />
-                <div className="p-6 space-y-2 lg:col-span-5">
-                    <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">Noster tincidunt reprimique ad pro</h3>
-                    <span className="text-xs text-gray-400">{new Date(blogs[0].published_at).toLocaleString()}</span>
-                    <p>{blogs[0].description}</p>
-                </div>
-            </a>
+            
             <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 {
                     blogs.map(blog => <BlogCard blog={blog} key={blog.id}></BlogCard>)
@@ -24,7 +16,7 @@ const Blogs = () => {
             </div>
            
         </div>
-    </section>
+
     );
 };
 
